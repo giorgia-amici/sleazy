72d29d6b3a679bc940c25580ebd6d5f08b157cb2
Mishal Islam <mishal.islam01@gmail.com>
2015-01-06 15:32:20 +0000

    written first test

---
+++ spec/features/tracks_feature_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'tracks' do
+  context 'no tunes have been added' do
+    scenario 'should say no tunes' do
+      visit '/tracks'
+      expect(page).to have_content 'Tuneless'
+    end
+  end
+end
