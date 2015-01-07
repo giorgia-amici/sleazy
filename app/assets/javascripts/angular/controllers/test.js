614c0560a890e0705f43178c4e03963800b53b69
Giorgia <giorgia1london@gmail.com>
2015-01-07 16:11:59 +0000

    user input

--- app/assets/javascripts/angular/controllers/test.js
+++ app/assets/javascripts/angular/controllers/test.js
@@ -1,18 +1,15 @@
 angular.module('app').controller('test', function($scope, $http){
-		// http://api.soundcloud.com/tracks/HateitOrLoveIt?client_id=0a3ed6b878677694faa3a0067c36d52c
-		// http://api.soundcloud.com/tracks/halo?client_id=0a3ed6b878677694faa3a0067c36d52c
-// 112436777
+	$scope.removeSpace = function(string){
+	  return string.split(' ').join(''); 
+	}
 	$scope.activateSearch = function(){
-		var songToSearch = $scope.toSearch
- 		console.log($scope.toSearch)
+		$scope.toSearch = $scope.removeSpace($scope.toSearchTemp)
+ 		var songToSearch = $scope.toSearch
 		var searchSong = $http.get("http://api.soundcloud.com/tracks/" + songToSearch + "?client_id=0a3ed6b878677694faa3a0067c36d52c")
  		searchSong
  		.success(function($scope){
  			var track = $scope.permalink_url
- 			SC.oEmbed(track, document.getElementById('player'));
+ 			SC.oEmbed(track, document.getElementById('wrapper'));
  		})
  	}
- 		// $scope.doSearch = function(){
-
- 		// }
 });
