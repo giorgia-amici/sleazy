c4b6d4a341e2dc0948edcb236a6fb7c19c08b902
Giorgia <giorgia1london@gmail.com>
2015-01-07 13:46:48 +0000

    we have a song

--- app/assets/javascripts/angular/controllers/test.js
+++ app/assets/javascripts/angular/controllers/test.js
@@ -1,2 +1,15 @@
-angular.module('app').controller('test', function($scope)
-{ console.log($scope); $scope.test = 'DIFF contrroller'});
+angular.module('app').controller('test', function($scope, $http){
+		var searchSong = $http.get("http://api.soundcloud.com/tracks/112436777.json?client_id=0a3ed6b878677694faa3a0067c36d52c")
+ 		searchSong.success(function($scope){
+ 			var track = $scope.permalink_url
+ 			SC.oEmbed(track, document.getElementById('player'));
+ 		})
+
+
+
+ 	// console.log($scope.song.stream_url)
+
+
+
+
+});
