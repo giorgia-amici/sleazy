589e783f4583a18783e012653b4258bd0bf9da42
Giorgia <giorgia1london@gmail.com>
2015-01-07 14:35:47 +0000

    surprise!

--- app/assets/javascripts/angular/controllers/test.js
+++ app/assets/javascripts/angular/controllers/test.js
@@ -1,15 +1,18 @@
 angular.module('app').controller('test', function($scope, $http){
-		var searchSong = $http.get("http://api.soundcloud.com/tracks/112436777.json?client_id=0a3ed6b878677694faa3a0067c36d52c")
- 		searchSong.success(function($scope){
+		// http://api.soundcloud.com/tracks/HateitOrLoveIt?client_id=0a3ed6b878677694faa3a0067c36d52c
+		// http://api.soundcloud.com/tracks/halo?client_id=0a3ed6b878677694faa3a0067c36d52c
+// 112436777
+	$scope.activateSearch = function(){
+		var songToSearch = $scope.toSearch
+ 		console.log($scope.toSearch)
+		var searchSong = $http.get("http://api.soundcloud.com/tracks/" + songToSearch + "?client_id=0a3ed6b878677694faa3a0067c36d52c")
+ 		searchSong
+ 		.success(function($scope){
  			var track = $scope.permalink_url
  			SC.oEmbed(track, document.getElementById('player'));
  		})
+ 	}
+ 		// $scope.doSearch = function(){
 
-
-
- 	// console.log($scope.song.stream_url)
-
-
-
-
+ 		// }
 });
