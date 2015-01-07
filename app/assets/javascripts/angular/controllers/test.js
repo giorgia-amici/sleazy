5adb4d6f9665b3c02f655324b0222fd194b0a9e8
Giorgia <giorgia1london@gmail.com>
2015-01-07 18:26:51 +0000

    wip

--- app/assets/javascripts/angular/controllers/test.js
+++ app/assets/javascripts/angular/controllers/test.js
@@ -1,15 +1,34 @@
 angular.module('app').controller('test', function($scope, $http){
+
+	SC.initialize({
+		client_id: '0a3ed6b878677694faa3a0067c36d52c'
+	})
+
 	$scope.removeSpace = function(string){
 	  return string.split(' ').join(''); 
 	}
 	$scope.activateSearch = function(){
 		$scope.toSearch = $scope.removeSpace($scope.toSearchTemp)
  		var songToSearch = $scope.toSearch
-		var searchSong = $http.get("http://api.soundcloud.com/tracks/" + songToSearch + "?client_id=0a3ed6b878677694faa3a0067c36d52c")
- 		searchSong
- 		.success(function($scope){
- 			var track = $scope.permalink_url
- 			SC.oEmbed(track, document.getElementById('wrapper'));
+ 		console.log(songToSearch)
+		var searchSong = $http.get("http://api.soundcloud.com/tracks.json?client_id=0a3ed6b878677694faa3a0067c36d52c&q=" + songToSearch + "&limit=10")
+ 		.success(function(songs){
+ 			songs.forEach(function(song){
+ 			var track = song.permalink_url
+ 			// SC.oEmbed(track, document.getElementById('wrapper'));
+ 			console.log(track)
+
+ 			})
+ 			// console.log(songs[0].permalink_url)
+ 			// console.log(searchSong)
  		})
  	}
 });
+
+
+
+// $scope.doRepos = function(){
+//     $scope.repoResults = searchRepos.query({
+//       q: $scope.searchTerm
+//     })   
+// };
