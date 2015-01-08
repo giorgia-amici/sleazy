71066cff29f70535f8b4040a5f539bc7bcef7ff8
ctrembath <claretrembath@ymail.com>
2015-01-08 18:07:21 +0000

    style

--- app/assets/javascripts/angular/controllers/search.js
+++ app/assets/javascripts/angular/controllers/search.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD:app/assets/javascripts/angular/controllers/test.js
-angular.module('app').controller('test', function($scope, $http) {
-=======
-console.log("")
-
 angular.module('app').controller('search', function($scope, $http){
->>>>>>> 40e0c9a309c8e9514863266d3067986e95b2ee73:app/assets/javascripts/angular/controllers/search.js
 
     SC.initialize({
         client_id: 'd1833a816c234ee6c77a76e948e9dbd1'
     })
-
-<<<<<<< HEAD:app/assets/javascripts/angular/controllers/test.js
     $scope.removeSpace = function(string) {
         return string.split(' ').join('');
     }
@@ -46,30 +38,3 @@ angular.module('app').controller('search', function($scope, $http){
 
 
 });
-=======
-	$scope.removeSpace = function(string){
-	  return string.split(' ').join(''); 
-	}
-	$scope.activateSearch = function(){
-		$scope.toSearch = $scope.removeSpace($scope.toSearchTemp)
- 		var songToSearch = $scope.toSearch
- 		// console.log(songToSearch)
-				var searchSong = $http.get("http://api.soundcloud.com/tracks.json?client_id=d1833a816c234ee6c77a76e948e9dbd1&q=" + songToSearch + "&limit=55")
-		 		.success(function(songs){
-				 			$scope.list = []
-				 			// console.log(songs)
-				 			songs.forEach(function(song){
-				 				// console.log(song)
-				 				
-				 				var track = song.permalink_url
-				 				$scope.list.push(song)
-				 			// firstTrack = songs[0].permalink_url
-		 			})
-				 			console.log($scope.list)
-				 				// console.log($scope.list)
-		 			// SC.oEmbed(firstTrack, document.getElementById('wrapper'));
-		 		})
- }
-
-});
->>>>>>> 40e0c9a309c8e9514863266d3067986e95b2ee73:app/assets/javascripts/angular/controllers/search.js
