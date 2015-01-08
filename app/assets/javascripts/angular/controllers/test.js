aa9920a8f5bd3be7ace07fb4c309d6018688f3f2
Giorgia <giorgia1london@gmail.com>
2015-01-08 12:37:46 +0000

    changes

--- app/assets/javascripts/angular/controllers/test.js
+++ app/assets/javascripts/angular/controllers/test.js
@@ -14,12 +14,16 @@ angular.module('app').controller('test', function($scope, $http){
 				var searchSong = $http.get("http://api.soundcloud.com/tracks.json?client_id=d1833a816c234ee6c77a76e948e9dbd1&q=" + songToSearch + "&limit=55")
 		 		.success(function(songs){
 				 			$scope.list = []
+				 			// console.log(songs)
 				 			songs.forEach(function(song){
-				 			var track = song.permalink_url
+				 				// console.log(song)
+				 				
+				 				var track = song.permalink_url
 				 				$scope.list.push(song)
 				 			// firstTrack = songs[0].permalink_url
 		 			})
-				 				console.log($scope.list)
+				 			console.log($scope.list)
+				 				// console.log($scope.list)
 		 			// SC.oEmbed(firstTrack, document.getElementById('wrapper'));
 		 		})
  }
