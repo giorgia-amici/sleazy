8c4911fe896c2fa130391530bf5dc71c32121b90
Giorgia <giorgia1london@gmail.com>
2015-01-13 12:08:44 +0000

    up-to-date

---
+++ app/assets/javascripts/angular/services/SearchSong.js
@@ -0,0 +1,73 @@
+angular.module('app').factory('searchSong', function($http){
+
+
+
+	SC.initialize({
+		client_id: 'd1833a816c234ee6c77a76e948e9dbd1'
+	})
+
+	$scope.removeSpace = function(string){
+	  return string.split(' ').join(''); 
+	}
+
+	$scope.activateSearch = function(){
+		$scope.toSearch = $scope.removeSpace($scope.toSearchTemp)
+ 		var songToSearch = $scope.toSearch
+		var searchSong = $http.get("http://api.soundcloud.com/tracks.json?client_id=d1833a816c234ee6c77a76e948e9dbd1&q=" + songToSearch + "&limit=55")
+ 		.success(function(songs){
+ 				$scope.songs = songs
+				$scope.list = []
+		 			songs.forEach(function(song){
+		 				song.playing = "false"
+			 			var track = song.permalink_url
+			 			$scope.list.push(song)
+		 		})
+		})
+		.error(function(){
+			console.log(error.status)
+		})
+	}
+
+	$scope.playStopSong = function(song){
+		SC.stream("/tracks/" + song.id, function(sound,error){
+			console.log(sound)
+			if(song.playing === "true"){
+				soundManager.stopAll();
+			 	song.playing = "false"
+			}
+		 	else{
+			  sound.play( 
+				 {onload: function() {
+      			if (this.readyState == 2) {
+        			console.log('ERROR')
+     				 }
+    				}}
+    					)
+				$scope.thisTitle = song.title
+				console.log($scope.thisTitle)
+				song.playing = "true"
+				// console.log(song.id)
+			}
+		
+		})
+	}
+	
+	$scope.test = {}	
+
+	$scope.submit = function(song){
+		$scope.test = {object: song.permalink_url, title: song.title }
+		var toSubmit = $scope.test
+		$http.post('/tracks', toSubmit)
+		.success(function(response) {
+
+		});
+		console.log(toSubmit)
+	}
+
+	$scope.stopSong = function(){
+		soundManager.stopAll();
+		song.playing = "false"
+	}
+
+
+})
